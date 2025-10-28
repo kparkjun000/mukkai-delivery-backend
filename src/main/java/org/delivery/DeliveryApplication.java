16b479c67f571b2f291ad424edb76866677780e5
kparkjun000 <kparkjun@gmail.com>
2025-10-28 16:51:37 +0900

    Add ComponentScan to load API controllers

--- src/main/java/org/delivery/DeliveryApplication.java
+++ src/main/java/org/delivery/DeliveryApplication.java
@@ -2,8 +2,10 @@ package org.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.delivery", "org.delivery.api", "org.delivery.storeadmin"})
 public class DeliveryApplication {
     public static void main(String[] args) {
         SpringApplication.run(DeliveryApplication.class, args);
