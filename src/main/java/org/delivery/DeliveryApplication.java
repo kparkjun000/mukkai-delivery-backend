417c3396da73eb6450e8b4a64860acf7896b4e96
kparkjun000 <kparkjun@gmail.com>
2025-10-28 17:23:43 +0900

    Build full service, load API module only with ComponentScan

--- src/main/java/org/delivery/DeliveryApplication.java
+++ src/main/java/org/delivery/DeliveryApplication.java
@@ -2,14 +2,10 @@ package org.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.delivery", "org.delivery.api"})
-@EntityScan(basePackages = "org.delivery.db")
-@EnableJpaRepositories(basePackages = "org.delivery.db")
+@ComponentScan(basePackages = {"org.delivery.config", "org.delivery.api"})
 public class DeliveryApplication {
     public static void main(String[] args) {
         SpringApplication.run(DeliveryApplication.class, args);
